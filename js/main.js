796f16983aa9d33735af1c8ed87b97c37bd2dc69
Mottel Zirkind <shachna@gmail.com>
2019-03-03 00:50:10 -0500

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
+//Run jQuery after the document is fully loaded.
 $(document).ready(
+    //The function that does the stuff.
     function () {
+        //Make the AJAX call
         $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
             method: "GET",
             dataType: "json"
-        }).done(function (data) {
-            $('#name').append(data.name);
-            console.log(data);
-            data._embedded.episodes.forEach(function (episode) {
-                $('#episodeList').append('<tr>'+
-                    '<td>' + episode.season + '</td>' +
-                    '<td>' + episode.number + '</td>' +
-                    '<td>' + episode.name + '</td>' +
-                    '<td>' + episode.summary + '</td>' +
-                    +' </tr>')
+        })
+        //After the data comes back, use this function
+            .done(
+                function (data) {
+                    //Add the name
+                    $('#name').append(data.name);
+                    //Add the episodes
+                    data._embedded.episodes.forEach(function (episode) {
+                        $('#episodeList').append('<tr>'+
+                            '<td>' + episode.season + '</td>' +
+                            '<td>' + episode.number + '</td>' +
+                            '<td>' + episode.name + '</td>' +
+                            '<td>' + episode.summary + '</td>' +
+                            +' </tr>')
             })
         })
     }
